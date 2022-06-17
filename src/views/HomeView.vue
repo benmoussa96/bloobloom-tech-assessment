f5355b3f6f4b4461d84ce837614b204d5326003e
benmoussa96 <ghaiethbmoussa@gmail.com>
2022-06-17 17:04:53 +0100

    Fixing home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="home-area section-padding-1 bg-gray breadcrumb-ptb-2">
-    <div class="container-fluid">
-      <div class="home-content text-center">
-        <img src="../assets/images/logo/bloobloom_logo.png" alt="banner" />
-        <div class="home-title">
-          <h2>Tech Assessment</h2>
+  <div class="home-area bg-gray-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="home-content text-center">
+            <img src="../assets/images/logo/bloobloom_logo.png" alt="banner" />
+            <h3>Tech Assessment!</h3>
+            <p>
+              By <a href="https://github.com/benmoussa96">Ghaieth BEN MOUSSA</a>
+            </p>
+            <div class="btn-style-1 text-center">
+              <a class="btn-border-black2" href="/collections/spectacles-women">
+                <div
+                  class="
+                    btn-viewmore-normal
+                    btn-ptb-3
+                    btn-viewmore-bg-transparent
+                    btn-viewmore-common
+                    btn-font-3
+                  "
+                >
+                  <span>Go To Glasses View</span>
+                </div>
+                <div
+                  class="
+                    btn-viewmore-hover
+                    btn-ptb-3
+                    btn-viewmore-common
+                    btn-hover-transition
+                    btn-font-3
+                  "
+                >
+                  <span>Go To Glasses View</span>
+                </div>
+              </a>
+            </div>
+          </div>
         </div>
-        <ul>
-          <li>
-            By <a href="https://github.com/benmoussa96">Ghaieth BEN MOUSSA</a>
-          </li>
-        </ul>
       </div>
     </div>
   </div>
